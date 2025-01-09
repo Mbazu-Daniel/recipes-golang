7eff355c1a9a0c09a21f0e3fbec78ec753af764f
Daniel Mbazu <mbazudaniel97@gmail.com>
2025-01-09 05:46:36 +0100

    delete recipe endpoint

--- main.go
+++ main.go
@@ -71,6 +71,29 @@ func UpdateRecipesHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipe)
 }
 
+// Delete Recipe
+func DeleteRecipesHandler(c *gin.Context) {
+	id := c.Param("id")
+	index := -1
+
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found",
+		})
+		return
+	}
+	recipes = append(recipes[:index], recipes[index+1:]...)
+	c.JSON(http.StatusOK, gin.H{
+		"msg": "Recipe has been deleted",
+	})
+}
+
 func init() {
 	recipes = make([]Recipe, 0)
 	file, _ := os.ReadFile("recipes.json")
@@ -82,6 +105,7 @@ func main() {
 	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipesHandler)
 	router.PUT("/recipes/:id", UpdateRecipesHandler)
+	router.DELETE("/recipes/:id", DeleteRecipesHandler)
 
 	router.Run()
 }
